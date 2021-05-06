ef679308a64d02feef9e52278f05296d324d621e
Wilmer H <wilmer.hernandez@gmail.com>
2021-05-06 15:07:06 -0500

    Quité el comentario anterior.

--- inserta_datos.php
+++ inserta_datos.php
@@ -1,6 +1,5 @@
 <?php
     include "connect.php";
-    ///Acá hice otro cambio
 
     $sql = "INSERT INTO inventario (ID, Nombre, Precio, Cantidad, fecha_reg)
     VALUES ('1445', 'Parlantes', '85000', '7', '06/05/2021')";
